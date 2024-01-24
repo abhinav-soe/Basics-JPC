17b0aa17545cfc518b48a660a9d99f7f249892a3
silent-shini <abhinavmishra2005.xyz@gmail.com>
2024-01-25 01:10:44 +0530

    my first day of playing with java and cpp
    had some serious problem with cpp

---
+++ cpp/readingInput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int value;
+    cout << "Enter Integer";
+    cin >> value;
+    cout << value;
+}
---
+++ java/readingInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class readingInput {
+    public static void main(String[] args) {
+        String a;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextLine();
+        System.out.println("HI");
+        System.out.println(a);
+        scan.close();
+    }
+
+}
